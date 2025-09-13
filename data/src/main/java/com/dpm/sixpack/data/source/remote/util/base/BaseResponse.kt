f955eac134f0ec546f78c54b86ac2e70b2f1dcba
beom84 <cjja00@konkuk.ac.kr>
2025-09-13 21:59:24 +0900

    [feat] #10 BaseResponse 구현

---
+++ data/src/main/java/com/dpm/sixpack/data/source/remote/util/base/BaseResponse.kt
@@ -0,0 +1,15 @@
+package com.dpm.sixpack.data.source.remote.util.base
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+data class BaseResponse<T>(
+    @SerialName("status")
+    val status: String ,
+    @SerialName("message")
+    val message: String ,
+    @SerialName ("timestamp")
+    val timestamp: String ,
+    @SerialName("data")
+    val data: T? = null,
+)
