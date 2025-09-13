344701a8f41a93bf49ff8646f06fab0cad27ee5c
beom84 <cjja00@konkuk.ac.kr>
2025-09-13 21:59:33 +0900

    [feat] #10 Api 상수 추가

---
+++ data/src/main/java/com/dpm/sixpack/data/source/remote/util/constant/ApiConstants.kt
@@ -0,0 +1,10 @@
+package com.dpm.sixpack.data.source.remote.util.constant
+
+object ApiConstants {
+    const val API = "api"
+    // 도메인 경로
+    const val RUNS = "runs"
+    const val GOALS = "goals"
+    const val SESSIONS = "sessions"
+    const val PLANS = "plans"
+}
