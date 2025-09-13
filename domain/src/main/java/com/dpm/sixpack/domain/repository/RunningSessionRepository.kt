c854c7d7cfb2e1b241106346eb40a018f08da956
yessorae <yessorae@gmail.com>
2025-09-14 02:55:10 +0900

    feat: RunningSession 관련 UseCase 추가

--- domain/src/main/java/com/dpm/sixpack/domain/repository/RunningSessionRepository.kt
+++ domain/src/main/java/com/dpm/sixpack/domain/repository/RunningSessionRepository.kt
@@ -4,9 +4,12 @@ import com.dpm.sixpack.domain.model.RealtimeRunningData
 import com.dpm.sixpack.domain.model.RunningSessionResult
 import com.dpm.sixpack.domain.usecase.SaveRealtimeRunningDataResult
 import com.dpm.sixpack.domain.util.DoRunResult
+import kotlinx.coroutines.flow.Flow
 
 interface RunningSessionRepository {
     //    suspend fun start(): DoRunResult<Unit>
+
+    suspend fun getRealtimeData(): Flow<DoRunResult<RealtimeRunningData>>
 
     suspend fun saveRealtimeData(data: RealtimeRunningData): DoRunResult<SaveRealtimeRunningDataResult.LocalResult>
 
---
+++ domain/src/main/java/com/dpm/sixpack/domain/usecase/GetRealtimeRunningDataUseCase.kt
@@ -0,0 +1,17 @@
+package com.dpm.sixpack.domain.usecase
+
+import com.dpm.sixpack.domain.model.RealtimeRunningData
+import com.dpm.sixpack.domain.repository.RunningSessionRepository
+import com.dpm.sixpack.domain.util.DoRunResult
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GetRealtimeRunningDataUseCase @Inject constructor(
+    private val repository: RunningSessionRepository
+) {
+    suspend operator fun invoke(): Flow<DoRunResult<RealtimeRunningData>> {
+        return repository.getRealtimeData()
+    }
+}
