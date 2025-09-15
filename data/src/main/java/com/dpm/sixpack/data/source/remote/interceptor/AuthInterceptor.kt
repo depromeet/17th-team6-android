642fc64fc7b73f711b44f24a3740a422119d8c1d
beom84 <cjja00@konkuk.ac.kr>
2025-09-15 17:04:25 +0900

    [feat] #17 AuthInterCeptor prefrence 적용

--- data/src/main/java/com/dpm/sixpack/data/source/remote/interceptor/AuthInterceptor.kt
+++ data/src/main/java/com/dpm/sixpack/data/source/remote/interceptor/AuthInterceptor.kt
@@ -1,43 +1,42 @@
 package com.dpm.sixpack.data.source.remote.interceptor
 
+import com.dpm.sixpack.domain.repository.UserPreferenceRepository
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import timber.log.Timber
 import javax.inject.Inject
 
 class AuthInterceptor
-    @Inject
-    constructor(
-//        TODO UserPrefrence 구현
-//    private val userPreferenceRepository: UserPreferenceRepository,
-    ) : Interceptor {
-        override fun intercept(chain: Interceptor.Chain): Response {
-            val originalRequest = chain.request()
+@Inject
+constructor(
+    private val userPreferenceRepository: UserPreferenceRepository,
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
 
-            val mockUserId = "1"
-//        TODO UserPrefrence 구현
-//        val userId = runBlocking {
-//            userPreferenceRepository.userPreferencesFlow
-//                .map { it.userId }
-//                .firstOrNull()
-//        }
-
-            Timber.d("UserIdInterceptor: Retrieved UserId -> $mockUserId")
-
-            val requestBuilder = originalRequest.newBuilder()
-
-            requestBuilder.addHeader(X_USER_ID, mockUserId)
-
-            //        TODO UserPrefrence 구현
-//        if (!userId.isNullOrBlank()) {
-//            requestBuilder.addHeader(X_USER_ID, userId)
-//        }
-
-            val newRequest = requestBuilder.build()
-            return chain.proceed(newRequest)
+        val userId = runBlocking {
+            try {
+                userPreferenceRepository.getUserId().toString()
+            } catch (e: Exception) {
+                Timber.e("AuthInterceptor: Failed to get UserId from DataStore. message: ${e.message}")
+                null
+            }
         }
 
-        companion object {
-            private const val X_USER_ID = "X-User-Id"
+        val requestBuilder = originalRequest.newBuilder()
+
+        if (userId != null) {
+            requestBuilder.addHeader(X_USER_ID, userId)
+        } else {
+            Timber.e("AuthInterceptor: UserId is null, proceeding without X-User-Id header.")
         }
+
+        val newRequest = requestBuilder.build()
+        return chain.proceed(newRequest)
     }
+
+    companion object {
+        private const val X_USER_ID = "X-User-Id"
+    }
+}
