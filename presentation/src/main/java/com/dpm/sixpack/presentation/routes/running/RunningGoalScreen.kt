14400a08bf10fa5e763316a17f442c84cb04f085
miller198 <miller198@naver.com>
2025-09-17 21:13:15 +0900

    [feature] 러닝 목표 스크린 뼈대

---
+++ presentation/src/main/java/com/dpm/sixpack/presentation/routes/running/RunningGoalScreen.kt
@@ -0,0 +1,12 @@
+package com.dpm.sixpack.presentation.routes.running
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+internal fun RunningGoalScreen(
+    modifier: Modifier = Modifier,
+//    viewModel: RunningGoalViewModel = hiltViewModel()
+) {
+}
