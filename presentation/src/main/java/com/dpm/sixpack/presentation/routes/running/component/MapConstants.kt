f54ed936111a6b583f68e3cf40c0a0e4d5f7a629
miller198 <miller198@naver.com>
2025-09-17 21:13:29 +0900

    [feature] 지도에 쓰이는 상수 추가

---
+++ presentation/src/main/java/com/dpm/sixpack/presentation/routes/running/component/MapConstants.kt
@@ -0,0 +1,21 @@
+package com.dpm.sixpack.presentation.routes.running.component
+
+import com.dpm.sixpack.core.permission.SixPackPermissions
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+
+object MapConstants {
+    const val DEFAULT_ZOOM = 16.0
+    const val MIN_ZOOM_LEVEL = 6.0
+    const val MAX_ZOOM_LEVEL = 19.0
+    const val MIN_LENGTH_PATH_ARRAY = 2
+    const val MIN_DISTANCE_BETWEEN_PATH = 5.0
+
+    val DEFAULT_CAMERA_POSITION =
+        CameraPosition(
+            LatLng(37.5665, 126.9780),
+            DEFAULT_ZOOM,
+        )
+
+    val MAP_PERMISSIONS = SixPackPermissions.Companion.LocationPermissions
+}
