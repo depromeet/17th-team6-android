af472f27dcfcb6dff9f44db165f03793bf11dbcd
beom84 <cjja00@konkuk.ac.kr>
2025-09-24 18:53:12 +0900

    [feat] #29 onboarding 공통 컴포넌트 구현

---
+++ presentation/src/main/java/com/dpm/sixpack/presentation/routes/onboarding/component/OnboardingNextButton.kt
@@ -0,0 +1,52 @@
+package com.dpm.sixpack.presentation.routes.onboarding.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dpm.sixpack.presentation.R
+import com.dpm.sixpack.presentation.common.components.DoRunDefaultButton
+import com.dpm.sixpack.presentation.theme.SixpackTheme
+
+@Composable
+fun OnboardingNextButton(
+    onClick: () -> Unit,
+    enabled: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val buttonColor = if (enabled) {
+        SixpackTheme.colors.gray0
+    } else {
+        SixpackTheme.colors.gray400
+    }
+
+    val contatinerColor = if (enabled) {
+        SixpackTheme.colors.blue600
+    } else {
+        SixpackTheme.colors.gray100
+    }
+
+    DoRunDefaultButton(
+        text = stringResource(R.string.button_default_next),
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        textColor = buttonColor,
+        containerColor = contatinerColor,
+        contentPadding = PaddingValues(top = 16.dp , bottom = 16.dp)
+    )
+}
+
+@Preview
+@Composable
+private fun OnboardingNextButtonPreview() {
+    OnboardingNextButton(
+        onClick = { },
+        enabled = true,
+        modifier = Modifier.padding(16.dp)
+    )
+}
