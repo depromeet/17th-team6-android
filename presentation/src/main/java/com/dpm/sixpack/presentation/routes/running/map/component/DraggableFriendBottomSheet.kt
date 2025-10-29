dcafce6195971b5e89bbf542c75f389c934a043e
miller198 <miller198@naver.com>
2025-10-29 19:49:05 +0900

    [chore] #55 import 정리

--- presentation/src/main/java/com/dpm/sixpack/presentation/routes/running/map/component/DraggableFriendBottomSheet.kt
+++ presentation/src/main/java/com/dpm/sixpack/presentation/routes/running/map/component/DraggableFriendBottomSheet.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
