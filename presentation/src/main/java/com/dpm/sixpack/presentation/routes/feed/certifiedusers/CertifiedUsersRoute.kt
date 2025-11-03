dc6526f84a8d87cf966136013c3bf258ce11be39
beom84 <cjja00@konkuk.ac.kr>
2025-11-03 12:56:17 +0900

    [feat] #60 UI 구현

---
+++ presentation/src/main/java/com/dpm/sixpack/presentation/routes/feed/certifiedusers/CertifiedUsersRoute.kt
@@ -0,0 +1,39 @@
+package com.dpm.sixpack.presentation.routes.feed.certifiedusers
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.dpm.sixpack.presentation.routes.feed.certifiedusers.contract.CertifiedUsersSideEffect
+import com.dpm.sixpack.presentation.routes.feed.certifiedusers.ui.CertifiedUsersScreen
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
+
+@Composable
+fun CertifiedUsersRoute(
+    date: String,
+    navigateToBack: () -> Unit,
+    navigateToUserProfile: (Long) -> Unit,
+    navigateToMyPage: () -> Unit,
+    viewModel: CertifiedUsersViewModel = hiltViewModel(),
+    ) {
+    val state by viewModel.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.loadCertifiedUsers(date)
+    }
+
+    viewModel.collectSideEffect { sideEffect ->
+        when (sideEffect) {
+            CertifiedUsersSideEffect.NavigateBack -> navigateToBack()
+            CertifiedUsersSideEffect.NavigateToMyPage -> navigateToMyPage()
+            is CertifiedUsersSideEffect.NavigateToUserPage ->
+                navigateToUserProfile(sideEffect.userId)
+        }
+    }
+
+    CertifiedUsersScreen(
+        state = state,
+        onIntent = viewModel::onIntent,
+    )
+}
