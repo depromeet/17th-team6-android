9bcd0d9cf79875d2efee455cfa369e656de8b315
miller198 <miller198@naver.com>
2025-11-05 16:22:18 +0900

    [feat] #70 친구 데이터 로드 실패 시 재시도 추가

--- presentation/src/main/java/com/dpm/sixpack/presentation/routes/running/map/friendsheet/FriendSheetLazyColumn.kt
+++ presentation/src/main/java/com/dpm/sixpack/presentation/routes/running/map/friendsheet/FriendSheetLazyColumn.kt
@@ -3,19 +3,31 @@ package com.dpm.sixpack.presentation.routes.running.map.friendsheet
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemKey
 import com.dpm.sixpack.presentation.common.model.FriendUiItem
+import com.dpm.sixpack.presentation.theme.SixpackTheme
 
 @Composable
 internal fun FriendSheetLazyColumn(
@@ -30,10 +42,48 @@ internal fun FriendSheetLazyColumn(
                 CircularProgressIndicator()
             }
         }
-        // 전체 새로고침 에러
+
         is LoadState.Error -> {
-            Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text("에러가 발생했습니다. (재시도 버튼 구현)")
+            Column(
+                modifier = modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "데이터를 불러오는 데 실패했습니다.",
+                    style = SixpackTheme.typography.b1Regular,
+                    color = SixpackTheme.colors.gray700,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                TextButton(
+                    onClick = {
+                        pagingItems.retry()
+                    },
+                    colors =
+                        ButtonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color.Transparent,
+                            disabledContainerColor = Color.Transparent,
+                            disabledContentColor = Color.Transparent,
+                        ),
+                ) {
+                    Row(
+                        modifier = Modifier.padding(all = 12.dp),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(
+                            text = "재시도",
+                            style = SixpackTheme.typography.b1Regular,
+                            color = SixpackTheme.colors.gray700,
+                        )
+                        Icon(
+                            imageVector = Icons.Default.Refresh,
+                            contentDescription = null,
+                            tint = SixpackTheme.colors.gray700,
+                        )
+                    }
+                }
             }
         }
         // 로드 성공 (및 비어있는 상태)
@@ -62,7 +112,6 @@ internal fun FriendSheetLazyColumn(
                             onAwakeClick = {
                                 onAwakeClick(it.userId)
                             },
-                            // 7. ViewModel Intent와 연결 (FriendListItem이 modifier를 받는다고 가정)
                             modifier =
                                 Modifier.clickable {
                                     onItemClick(it.userId)
